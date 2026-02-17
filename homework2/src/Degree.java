7a6a543a0f5d0790b2bbafc3920da1ed0965221c
okkonie <okko.niemikorpi1@gmail.com>
2026-02-17 13:15:01 +0200

    -

--- homework2/src/Degree.java
+++ homework2/src/Degree.java
@@ -3,5 +3,48 @@ public class Degree {
   private int count;
   private String degreeTitle = ConstantValues.NO_TITLE;
   private String titleOfThesis = ConstantValues.NO_TITLE;
-  private StudentCourse[] myCourses;
+  private StudentCourse[] myCourses = new StudentCourse[MAX_COURSES];
+
+  public StudentCourse[] getCourses(){
+    return myCourses;
+  }
+
+  public void addStudentCourses(StudentCourse[] courses){
+    for(StudentCourse course : courses){
+      addStudentCourse(course);
+    }
+  }
+
+  public boolean addStudentCourse(StudentCourse course){
+    if(count < MAX_COURSES && course != null){
+      myCourses[count + 1] = course;
+      return true;
+    }
+
+    return false;
+  }
+
+  public String getDegreeTitle() {
+    return degreeTitle;
+  }
+
+  public void setDegreeTitle(String degreeTitle) {
+    if(degreeTitle != null) {
+      this.degreeTitle = degreeTitle;
+    }
+  }
+
+  public String getTitleOfThesis() {
+    return titleOfThesis;
+  }
+
+  public void setTitleOfThesis(String titleOfThesis) {
+    if(degreeTitle != null){
+      this.titleOfThesis = titleOfThesis;
+    }
+  }
+
+  public double getCreditsByBase(Character base){
+    
+  }
 }
