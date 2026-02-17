bf3619f2a0a3091c3dab9e553f95628085b6a845
okkonie <okko.niemikorpi1@gmail.com>
2026-02-17 13:14:45 +0200

    -

---
+++ homework2/src/Degree.java
@@ -0,0 +1,7 @@
+public class Degree {
+  static final int MAX_COURSES = 50;
+  private int count;
+  private String degreeTitle = ConstantValues.NO_TITLE;
+  private String titleOfThesis = ConstantValues.NO_TITLE;
+  private StudentCourse[] myCourses;
+}
--- homework2/src/StudentCourse.java
+++ homework2/src/StudentCourse.java
@@ -1,7 +1,13 @@
+import java.time.Year;
+
 public class StudentCourse {
   private Course course;
   private int gradeNum;
-  private int yearCompleted;
+  private int yearCompleted = 0;
+
+  private int getCurrentYear(){
+    return Year.now().getValue();
+  }
 
   public Course getCourse() {
     return course;
@@ -15,9 +21,55 @@ public class StudentCourse {
     return gradeNum;
   }
 
-  protected void setGrade(int gradeNum) {
-    course.isNumericGrade()
-    this.gradeNum = gradeNum;
+  protected void setGrade(final int gradeNum) {
+    if(checkGradeValidity(gradeNum)){
+      this.gradeNum = gradeNum;
+      if(yearCompleted == 0){
+        this.yearCompleted = getCurrentYear();
+      }
+    }
+  }
+
+  private boolean checkGradeValidity(int gradeNum){
+    if(
+      (gradeNum >= ConstantValues.MIN_GRADE && gradeNum <= ConstantValues.MAX_GRADE)
+      || gradeNum == ConstantValues.GRADE_FAILED 
+      || gradeNum == ConstantValues.GRADE_ACCEPTED
+    ){
+      return true;
+    }
+    return false;
+  }
+
+  public boolean isPassed(){
+    int grade = getGradeNum();
+    if(grade == 0 || grade == 'F'){
+      return false;
+    }
+    return true;
+  }
+
+  public int getYear(){
+    return yearCompleted;
+  }
+
+  public void setYear(final int year){
+    int currentYear = getCurrentYear();
+
+    if(year >= 2000 && year <= currentYear){
+      this.yearCompleted = year;
+    }
+  }
+
+  @Override
+  public String toString() {
+    int grade = getGradeNum();
+    String gradeString = grade == 0 ? "\"Not graded\"" : Integer.toString(grade);
+    return String.format(
+      "[%s, (%5.2f cr), \"%s\". %s, period: %d.] Year: %d, Grade: %s.]", 
+      course.getCourseCode(), course.getCredits(), course.getName(), 
+      course.getCourseTypeString(), course.getPeriod(), getYear(), gradeString
+    );
   }
 
   public StudentCourse(){}
@@ -27,7 +79,15 @@ public class StudentCourse {
     final int yearCompleted
   ){
     setCourse(course);
-    setGradeNum(gradeNum);
-    setYearCompleted(yearCompleted);
+    setGrade(gradeNum);
+    setYear(yearCompleted);
+  }
+
+  public static void main(String[] args) {
+    Course x = new Course("a", 123, 'A', 1, 3, 5000.0, true);
+
+    StudentCourse y = new StudentCourse(x, 2, 2021);
+
+    System.out.println(y);
   }
 }
